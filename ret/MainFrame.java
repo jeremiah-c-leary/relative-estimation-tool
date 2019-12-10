586ca88d858a15142c1cc2c68a1cd9049f841550
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-12-10 15:33:31 -0600

    Adding in row painting.

--- ret/MainFrame.java
+++ ret/MainFrame.java
@@ -17,12 +17,21 @@ public class MainFrame extends JFrame {
     private JLabel labelDescription;
     private JLabel labelPercent;
     private JLabel labelHours;
-    private JFormattedTextField hourCell;
     private static int row_height = 20;
 
     ArrayList<Integer> numHours = new ArrayList<Integer>();
     private NumberFormat hourFormat;
-    private JFormattedTextField hourField;
+    private JFormattedTextField hourCell;
+
+    private NumberFormat nameFormat;
+    private JFormattedTextField nameCell;
+    private NumberFormat descriptionFormat;
+    private JFormattedTextField descriptionCell;
+    private NumberFormat percentFormat;
+    private JFormattedTextField percentCell;
+    private JLabel labelCell;
+
+    private Task topTask;
 
     public MainFrame() {
         super("Relative Estimation Tool");
@@ -31,6 +40,14 @@ public class MainFrame extends JFrame {
         initializeHours();
 
         paintHeader();
+
+        topTask = new Task();
+        topTask.setPercent(100);
+        topTask.setName("Enter Task Name");
+        topTask.setDescription("Enter Task Description");
+        topTask.setHours(numHours);
+
+        paintRows();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
@@ -43,6 +60,64 @@ public class MainFrame extends JFrame {
         numHours.add(0);
         numHours.add(0);
     }
+
+    private void paintRows() {
+
+        ArrayList<Task> allTasks = new ArrayList<Task>(topTask.getRecursiveSubtasks());
+        for(int i=0;i<allTasks.size();i++) {
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.gridheight = 1;
+            gbc.gridx = 0;
+            gbc.gridy = i + 2;
+ 
+            nameCell = new JFormattedTextField(nameFormat); 
+            nameCell.setBackground(Color.PINK);
+            nameCell.setOpaque(true);
+            nameCell.setPreferredSize(new Dimension(200,row_height));
+            nameCell.setValue(allTasks.get(i).getName());
+            add(nameCell, gbc);
+
+            descriptionCell = new JFormattedTextField(nameFormat); 
+            descriptionCell.setBackground(Color.PINK);
+            descriptionCell.setOpaque(true);
+            descriptionCell.setPreferredSize(new Dimension(500,row_height));
+            descriptionCell.setValue(allTasks.get(i).getDescription());
+            gbc.gridx = 1;
+            gbc.gridy = i + 2;
+            add(descriptionCell, gbc);
+
+            percentCell = new JFormattedTextField(nameFormat); 
+            percentCell.setBackground(Color.PINK);
+            percentCell.setOpaque(true);
+            percentCell.setPreferredSize(new Dimension(100,row_height));
+            percentCell.setValue(allTasks.get(i).getPercent());
+            gbc.gridheight = 1;
+            gbc.gridx = 2;
+            gbc.gridy = i + 2;
+            add(percentCell, gbc);
+
+            ArrayList<Integer> myHours = new ArrayList<Integer>(allTasks.get(i).getCalculatedHours());
+
+            for(int j=0;j<myHours.size();j++) {
+                gbc.gridheight = 1;
+                gbc.gridwidth = 1;
+                gbc.gridx = j + 3;
+                gbc.gridy = i + 2;
+                gbc.fill = GridBagConstraints.HORIZONTAL;
+                labelCell = new JLabel(Integer.toString(myHours.get(j)));
+                labelCell.setBackground(Color.PINK);
+                labelCell.setOpaque(true);
+                labelCell.setHorizontalAlignment(JLabel.CENTER);
+                labelCell.setPreferredSize(new Dimension(100, row_height));
+                add(labelCell, gbc);
+            }
+        }
+    }
+
+//*****************************************************************************
+// Header methods
+//*****************************************************************************
 
     private void paintHeader() {
         paintTask();
